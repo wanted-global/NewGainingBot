50a7a17348f76e8d467577c7a08a376725581002
Wanted™ <73367890+wanted-global@users.noreply.github.com>
2021-03-23 18:46:10 +0200

    Add files via upload

---
+++ nuoton7797.php
@@ -0,0 +1,39 @@
+<?php
+
+ob_start();
+
+$API_KEY = '1624545639:AAFpe0PoLSiJhKKPqDtnu8WqKawCMRcvVsk';
+define('API_KEY',$API_KEY);
+function bndr($method,$datas=[]{
+$url = "https://api.telegram.org/bot".API_KEY."/".$method;
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+$res = curl_exec($ch);
+if(curl_error($ch)){
+var_dump(curl_error($ch));
+}else{
+return json_decode($res);
+}
+}
+
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$text = $ message->text;
+$chat_id = $message->chat->id
+
+if($text =='/start'{
+bndr('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>'أهلاً بكم في الفهرس، لا يزال البوت قيد التطوير، لا تقم بحظر البوت رجاءً كي يصلك تنبيه عند تفعيل البوت'
+]);
+}
+if($text =='/help'{
+bndr('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>'هدف البوت هو وضع فهرس لأغلبية القنوات العربية الهادفة والمسلية ووضعها أمام المستخدمين ليتمكنوا من الاشتراك بالقنوات التي تهمهم بدلا من البحث عنها عشوائيا، إن موعد انتهاء تطوير البوت غير محدد بعدد نظراً للوقت الطويل الذي تستهلكه البرمجة'
+]);
+}
+
+?>
